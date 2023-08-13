0b00ff27d15f1580670da5b472a15e4732a61fb0
AngelinaCholak <127952572+AngelinaCholak@users.noreply.github.com>
2023-08-13 21:53:33 +0400

    hw-6

--- js/task-07.js
+++ js/task-07.js
@@ -1,11 +1,13 @@
 const inputEl = document.querySelector("#font-size-control");
 const textEl = document.querySelector("#text");
 
+textEl.style.fontSize = inputEl.value  +'px';
 
-inputEl.fontSize.addEventListener('input', () => {
+inputEl.addEventListener('input', () => {
     const fontSize = inputEl.value + 'px';
     textEl.style.fontSize = fontSize;
 });
+
 
 
 
